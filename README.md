e7bb7310e7f5f84ae0798002ddb35d961c5cbc59
Animesh Manglik <amanglik@whalerockindustries.com>
2016-12-09 11:46:15 -0800

    Swift: Finger Count Demo

---
+++ README.md
@@ -0,0 +1,8 @@
+Swift Development
+
+## Finger Count
+
+Basic Iphone 6 APP
+	.Guesses the number of finger
+	.Based on random number
+	.No AutoLayout for now
--- fingerCount/ViewController.swift
+++ fingerCount/ViewController.swift
@@ -10,9 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fingerInput: UITextField!
+    @IBOutlet weak var fingerOutput: UILabel!
+
+    let outputText = "You guessed: "
+    let outputText2 = "Fingers holding: "
+    
+    @IBAction func submit(_ sender: AnyObject) {
+        let randomNum:UInt32 = arc4random_uniform(6)
+        let fingerGuess:Int = Int(randomNum)
+
+        if ((fingerInput.text!) == "") {
+            fingerOutput.text = "Enter Something"
+            return
+        }
+
+        let fingerInputText = fingerInput.text!
+        let fingerGuessInput:Int = Int(fingerInputText)!
+        
+        var outputText = self.outputText + fingerInputText + "\n"
+        outputText += self.outputText2 + String(fingerGuess) + "\n"
+
+        if (fingerGuess == fingerGuessInput) {
+            outputText += "Wohoo Congratulations!"
+        } else {
+            outputText += "Try Again :("
+        }
+        
+        fingerInput.text = ""
+        fingerOutput.text = outputText
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        fingerOutput.text = "Input Number and guess the answer?"
     }
 
     override func didReceiveMemoryWarning() {
